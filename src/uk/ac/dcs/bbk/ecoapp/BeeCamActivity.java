11a8057fb27ef15d7c2cbb1700838cd5160a4882
George Roussos <g.roussos@acm.org>
2012-02-16 10:47:21 +0000

    added main activity templates

---
+++ src/uk/ac/dcs/bbk/ecoapp/BeeCamActivity.java
@@ -0,0 +1,13 @@
+package uk.ac.dcs.bbk.ecoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BeeCamActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
