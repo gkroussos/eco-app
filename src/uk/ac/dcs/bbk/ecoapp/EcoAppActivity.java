81f5ddb468e052638855e303f32b0f2db7288a25
Quangui Huang <Giorgio.H@Gmail.com>
2012-03-15 12:45:53 +0000

    Upgrade the Loading Screen with playing background sound during startup, modify the method for network state determination.

--- src/uk/ac/dcs/bbk/ecoapp/EcoAppActivity.java
+++ src/uk/ac/dcs/bbk/ecoapp/EcoAppActivity.java
@@ -1,8 +1,10 @@
 package uk.ac.dcs.bbk.ecoapp;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Properties;
 
@@ -17,10 +19,14 @@ import org.xml.sax.XMLReader;
 import com.google.android.apps.analytics.GoogleAnalyticsTracker;
 import android.app.Activity;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources.NotFoundException;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.media.MediaPlayer;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.sax.Element;
@@ -51,131 +57,144 @@ public class EcoAppActivity extends Activity {
 	private static int GET_VERSION = 0;
 	private static int GET_DATA = 1;
 	private GoogleAnalyticsTracker tracker;
-	protected int splashTime = 3000; // Milliseconds to display the loading screen
+	protected int splashTime = 4000; // Milliseconds to display the loading screen
+	private static int CONNECT_TIMEOUT = 4000;
 	private boolean networkAvailable = true; // Network availability
+	private MediaPlayer mp;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.ecoapp);
-		
-		// For Ray
-		tracker = GoogleAnalyticsTracker.getInstance();
-		tracker.startNewSession("UA-29500489-1", this);
-		tracker.trackPageView("UserOpenApp");
-		tracker.trackEvent("Clicks", // Category
-				"Button", // Action
-				"clicked", // Label
-				77); // Value
-		tracker.dispatch();
 
 		// the progress bar
 		updateDbBar = (ProgressBar) this.findViewById(R.id.updateDbBar);
 		updateDbBar.setIndeterminate(true);
 
-		// get the XML data properties
-		Properties p = new Properties();
-		try {
-			p.load(this.getResources().openRawResource(R.raw.data));
-			versionUrl = p.getProperty("url.version");
-			Log.i("data version url", versionUrl);
+		// play background sound
+		mp = new MediaPlayer();
+		mp = MediaPlayer.create(EcoAppActivity.this, R.raw.startup);
+		mp.start();
 
-			dataUrl = p.getProperty("url.data");
-			Log.i("data url", dataUrl);
+		// check the network state
+		networkAvailable = this.checkNetworkAvailability(EcoAppActivity.this);
 
-		} catch (NotFoundException e1) {
-			// TODO Auto-generated catch block
-			Log.e("Property file not found", e1.getMessage());
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			Log.e("Property file IO error", e1.getMessage());
-		}
+		if (networkAvailable) {
 
-		// get data version
-		xmlParser(versionUrl, GET_VERSION);
+			// For Ray
+			tracker = GoogleAnalyticsTracker.getInstance();
+			tracker.startNewSession("UA-29500489-1", this);
+			tracker.trackPageView("UserOpenApp");
+			tracker.trackEvent("Clicks", // Category
+					"Button", // Action
+					"clicked", // Label
+					77); // Value
+			tracker.dispatch();
 
-		try {
-			eOpenHelper = new EcoSQLiteOpenHelper(this);
-			sqlDB = eOpenHelper.getWritableDatabase();
+			// get the XML data properties
+			Properties p = new Properties();
+			try {
+				p.load(this.getResources().openRawResource(R.raw.data));
+				versionUrl = p.getProperty("url.version");
+				Log.i("data version url", versionUrl);
 
-			double localVersion = 0;
-			if (sqlDB.isOpen()) {
-				// fetch the local sites version
-				Cursor cursor = sqlDB.query(EcoSQLiteOpenHelper.TABLE_SITES_VERSION, new String[] { EcoSQLiteOpenHelper.SITE_VERSION }, null, null, null, null, null);
-				if (cursor != null) {
-					cursor.moveToFirst();
-					if (!cursor.isAfterLast()) {
-						do {
-							localVersion = cursor.getDouble(0);
-						} while (cursor.moveToNext());
+				dataUrl = p.getProperty("url.data");
+				Log.i("data url", dataUrl);
+
+			} catch (NotFoundException e1) {
+				// TODO Auto-generated catch block
+				Log.e("Property file not found", e1.getMessage());
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				Log.e("Property file IO error", e1.getMessage());
+			}
+
+			// get data version
+			xmlParser(versionUrl, GET_VERSION);
+
+			try {
+				eOpenHelper = new EcoSQLiteOpenHelper(this);
+				sqlDB = eOpenHelper.getWritableDatabase();
+
+				double localVersion = 0;
+				if (sqlDB.isOpen()) {
+					// fetch the local sites version
+					Cursor cursor = sqlDB.query(EcoSQLiteOpenHelper.TABLE_SITES_VERSION, new String[] { EcoSQLiteOpenHelper.SITE_VERSION }, null, null, null, null, null);
+					if (cursor != null) {
+						cursor.moveToFirst();
+						if (!cursor.isAfterLast()) {
+							do {
+								localVersion = cursor.getDouble(0);
+							} while (cursor.moveToNext());
+						}
+						cursor.close();
 					}
-					cursor.close();
 				}
-			}
 
-			Log.i("localVersion", localVersion + "");
+				Log.i("localVersion", localVersion + "");
 
-			double siteVersion = 0;
-			if (versionString != null) {
-				siteVersion = Double.parseDouble(versionString);
-				Log.i("siteVersion", versionString);
-			}
+				double siteVersion = 0;
+				if (versionString != null) {
+					siteVersion = Double.parseDouble(versionString);
+					Log.i("siteVersion", versionString);
+				}
 
-			// the local data version is out of date
-			if (siteVersion > 0 && localVersion < siteVersion) {
+				// the local data version is out of date
+				if (siteVersion > 0 && localVersion < siteVersion) {
 
-				updateDbBar.setIndeterminate(false);
+					updateDbBar.setIndeterminate(false);
 
-				// get data
-				xmlParser(dataUrl, GET_DATA);
+					// get data
+					xmlParser(dataUrl, GET_DATA);
 
-				// there do have some new sites data
-				if (sitesList.size() > 0) {
-					updateDbBar.setMax(sitesList.size());
+					// there do have some new sites data
+					if (sitesList.size() > 0) {
+						updateDbBar.setMax(sitesList.size());
 
-					int rsPro = 0;
+						int rsPro = 0;
 
-					// reset the database
-					eOpenHelper.onCreate(sqlDB);
-					for (int i = 0; i < sitesList.size(); i++) {
+						// reset the database
+						eOpenHelper.onCreate(sqlDB);
+						for (int i = 0; i < sitesList.size(); i++) {
 
-						Site loc = sitesList.get(i);
-						ContentValues cv = new ContentValues();
-						cv.put(EcoSQLiteOpenHelper.SITE_NAME, loc.getName());
-						cv.put(EcoSQLiteOpenHelper.SITE_DESCRIPTION, loc.getDescription());
-						cv.put(EcoSQLiteOpenHelper.SITE_TYPE, loc.getType());
-						cv.put(EcoSQLiteOpenHelper.SITE_LATITUDE, loc.getLatitude());
-						cv.put(EcoSQLiteOpenHelper.SITE_LONGITUDE, loc.getLongitude());
-						cv.put(EcoSQLiteOpenHelper.SITE_ICON, loc.getIcon());
-						long rs = sqlDB.insert(EcoSQLiteOpenHelper.TABLE_SITES, null, cv);
+							Site loc = sitesList.get(i);
+							ContentValues cv = new ContentValues();
+							cv.put(EcoSQLiteOpenHelper.SITE_NAME, loc.getName());
+							cv.put(EcoSQLiteOpenHelper.SITE_DESCRIPTION, loc.getDescription());
+							cv.put(EcoSQLiteOpenHelper.SITE_TYPE, loc.getType());
+							cv.put(EcoSQLiteOpenHelper.SITE_LATITUDE, loc.getLatitude());
+							cv.put(EcoSQLiteOpenHelper.SITE_LONGITUDE, loc.getLongitude());
+							cv.put(EcoSQLiteOpenHelper.SITE_ICON, loc.getIcon());
+							long rs = sqlDB.insert(EcoSQLiteOpenHelper.TABLE_SITES, null, cv);
 
-						if (rs != -1) {
-							updateDbBar.setProgress(i);
-							rsPro++;
+							if (rs != -1) {
+								updateDbBar.setProgress(i);
+								rsPro++;
+							}
+
 						}
 
+						// all new data has been inserted into tables
+						if (rsPro == sitesList.size()) {
+							// update the local data version
+							ContentValues vcv = new ContentValues();
+							vcv.put(EcoSQLiteOpenHelper.SITE_VERSION, siteVersion);
+							sqlDB.insert(EcoSQLiteOpenHelper.TABLE_SITES_VERSION, null, vcv);
+
+						}
 					}
 
-					// all new data has been inserted into tables
-					if (rsPro == sitesList.size()) {
-						// update the local data version
-						ContentValues vcv = new ContentValues();
-						vcv.put(EcoSQLiteOpenHelper.SITE_VERSION, siteVersion);
-						sqlDB.insert(EcoSQLiteOpenHelper.TABLE_SITES_VERSION, null, vcv);
-
-					}
 				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				Log.e("Eco App Error", e.getMessage());
+			} finally {
+				if (sqlDB != null && sqlDB.isOpen()) {
+					sqlDB.close();
+				}
+			}
 
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			Log.e("Eco App Error", e.getMessage());
-		} finally {
-			if (sqlDB != null && sqlDB.isOpen()) {
-				sqlDB.close();
-			}
 		}
-
 		gotoTargetActivity();
 
 	}
@@ -195,7 +214,10 @@ public class EcoAppActivity extends Activity {
 			SAXParser parser = factory.newSAXParser();
 			XMLReader xmlreader = parser.getXMLReader();
 			URL url = new URL(urlString);
-			InputSource is = new InputSource(url.openStream());
+			URLConnection con = url.openConnection();
+			con.setConnectTimeout(CONNECT_TIMEOUT);
+			InputStream in = con.getInputStream();
+			InputSource is = new InputSource(in);
 			if (type == EcoAppActivity.GET_VERSION) {
 				xmlreader.setContentHandler(getVersionRootElement().getContentHandler());
 			} else if (type == EcoAppActivity.GET_DATA) {
@@ -214,8 +236,7 @@ public class EcoAppActivity extends Activity {
 			Log.e("MalformedURLException", e.getMessage());
 		} catch (IOException e) {// cannot access the Internet
 			// TODO Auto-generated catch block
-			networkAvailable = false;
-			Log.e("No network", e.getMessage());
+			Log.e("XML file is not accessable", e.getMessage());
 		}
 	}
 
@@ -227,13 +248,13 @@ public class EcoAppActivity extends Activity {
 		new Handler().postDelayed(new Runnable() {
 			@Override
 			public void run() {
-				//finish();
+
 				if (networkAvailable) {
 					startActivity(new Intent(EcoAppActivity.this, mapActivity));
 				} else {
 					startActivity(new Intent(EcoAppActivity.this, listActivity));
 				}
-				
+
 				EcoAppActivity.this.finish();
 			}
 		}, splashTime);
@@ -340,6 +361,36 @@ public class EcoAppActivity extends Activity {
 		});
 
 		return rootElement;
+
+	}
+
+	/**
+	 * check network availability
+	 * 
+	 * @param activity
+	 *            - current activity
+	 * @return boolean - true: network available; false: no network
+	 */
+	public boolean checkNetworkAvailability(Activity activity) {
+
+		ConnectivityManager cManager = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo nInfo = cManager.getActiveNetworkInfo();
+		if (nInfo == null || !nInfo.isConnected()) {
+			return false;
+		} else {
+			return true;
+		}
+
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// TODO Auto-generated method stub
+		if (mp != null) {
+			mp.release();
+			mp = null;
+		}
 
 	}
 
